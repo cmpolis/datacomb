45bbb0983d4150c8d3d1866b94ac7286003b3963
Chris Polis <cmpolis@gmail.com>
2015-08-09 20:26:39 -0700

    project setup: npm deps, demo dir and index.html, test dir

---
+++ test/meta-test.js
@@ -0,0 +1,20 @@
+//
+//
+//
+
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var should = chai.should();
+chai.use(sinonChai);
+
+//
+describe('Testing environment', function() {
+
+  //
+  it('should run tests w/ `should` syntax', function() {
+    var meaningOfLife = 42;
+    meaningOfLife.should.equal(42);
+  });
+
+});
